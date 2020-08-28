f44d379cee1f3b0914841acb0a83b3f27a68563f
Jon Wear <jonwear@fastmail.fm>
2020-08-27 22:11:45 -0400

    basic stubbing of the needed functions

--- v1/usermanage/types.go
+++ v1/usermanage/types.go
@@ -1,13 +1,15 @@
 package um
 
-const ErrorTypeClient = 4
-const ErrorTypeServer = 5
+const errorTypeClient = 4
+const errorTypeServer = 5
 
+//ServiceError is a customer error type to help differentiate between client and server side errors
 type ServiceError struct {
 	Message   string
 	ErrorType int
 }
 
+//NewServiceError accepts a 4 or a 5 and a message to create a ServiceError
 func NewServiceError(errortype int, message string) ServiceError {
 	if errortype != 4 && errortype != 5 {
 		panic("Invalid Error Type " + string(errortype))
